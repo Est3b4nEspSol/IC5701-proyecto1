f4bdbe8ffd8cdc9a2b629f87cecaba927bc7fbf3
Esteban Espinoza <166446894+Est3b4nEspSol@users.noreply.github.com>
2025-12-26 01:09:36 -0600

    Refactor Main class into MainJFlexCup
    Besides changing the name, I added a function to do tests.

--- Java/src/main/java/generated/MainJFlexCup.java
+++ Java/src/main/java/generated/MainJFlexCup.java
@@ -1,6 +1,6 @@
 package generated;
 
-import java.io.Reader;
+import java_cup.Lexer;
 import java_cup.internal_error;
 import java_cup.parser;
 import java_cup.runtime.Symbol;
@@ -10,12 +10,10 @@ import jflex.exceptions.SilentExit;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java_cup.parser;
 
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 
-public class Main {
+public class MainJFlexCup {
 
     public void iniLexerParser(String rutaLexer, String rutaParser) throws internal_error, Exception{
         GenerateLexer(rutaLexer);
@@ -32,8 +30,30 @@ public class Main {
         java_cup.Main.main(strArr);
     }
 
+    public void prueba1(String rutaScanear) throws IOException{
+        Reader reader = new BufferedReader(new FileReader(rutaScanear));
+        reader.read();
+        Lexer lex = new Lexer(reader);
+        int i = 0;
+        Symbol token;
+        while(true)
+        {
+            token = lex.next_token();
+            if(token.sym != 0){
+                System.out.println("token: "+ ", valor: " +(token.value == null?lex.yytext():token.value.toString()));
+            }
+            else {
+                System.out.println("Cantidad de lexemas encontrados: " + i);
+                return;
+            }
+            i++;
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
         String input = "12 + 34 + 5";
+        System.out.println(System.getProperty("user.dir"));
 
         Lexer lexer = new Lexer(new StringReader(input));
         parser parser = new parser(lexer);
