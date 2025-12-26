3e17c6e95d2e24d3ca8a2afff55033a161c89fc9
Esteban Espinoza <166446894+Est3b4nEspSol@users.noreply.github.com>
2025-12-26 01:08:11 -0600

    Arreglo de rutas
    Arreglé las rutas para la eliminación/creación de archivos, pero sigue fallando en la generación del lexer por jflex.Main.generate. Además, incluí una parte de pruebas.

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,7 +1,6 @@
-import generated.Main;
+import generated.MainJFlexCup;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
@@ -9,31 +8,43 @@ public class App {
 
     public static void GenerarLexerParser() throws Exception{
         String basePath, fullPathLexer, fullPathparser, jlexer, jparser, jlexerCarpeta;
-        Main main;
+        MainJFlexCup mfjc;
 
         basePath = System.getProperty("user.dir");
         //.java de parser y lexer
         jlexer = "Lexer.java";
         jparser = "Parser.java";
 
-        main = new Main();
+        mfjc = new MainJFlexCup();
 
         Files.deleteIfExists(Paths.get(basePath + "\\src\\lexer\\lexer.flex"));
 
-        fullPathLexer = basePath + "\\src\\main\\java\\generated\\lexer.flex";
-        fullPathparser = basePath + "\\src\\parser\\parser.cup";
+        fullPathLexer = basePath + "\\Java\\src\\main\\java\\generated\\lexer.flex";
+        fullPathparser = basePath + "\\Java\\src\\parser\\parser.cup";
         jparser = "ParserIni.java";
         jlexer = "Lexer.java";
         jlexerCarpeta = "generated";
 
-        Files.deleteIfExists(Paths.get(basePath+ "\\src\\main\\java\\generated\\" + jparser));
-        Files.deleteIfExists(Paths.get(basePath+ "\\src\\main\\java\\generated\\" + jlexer));
+        Files.deleteIfExists(Paths.get(basePath+ "\\Java\\src\\main\\java\\generated\\" + jparser));
+        Files.deleteIfExists(Paths.get(basePath+ "\\Java\\src\\main\\java\\generated\\" + jlexer));
 
-        main.iniLexerParser(fullPathLexer, fullPathparser);
+        mfjc.iniLexerParser(fullPathLexer, fullPathparser);
 
-        Files.move(Paths.get(basePath+"\\sym.java"), Paths.get(basePath + "\\src\\main\\java\\generated\\sym.java"));
-        Files.move(Paths.get(basePath+"\\"+ jparser), Paths.get(basePath + "\\src\\main\\java\\generated\\"+ jparser));
-        Files.move(Paths.get(basePath+"\\src\\" + jlexerCarpeta + "\\" + jlexer), Paths.get(basePath + "\\src\\main\\java\\generated\\" + jlexer));
+        Files.move(Paths.get(basePath+"\\sym.java"), Paths.get(basePath + "\\Java\\src\\main\\java\\generated\\sym.java"));
+        Files.move(Paths.get(basePath+"\\"+ jparser), Paths.get(basePath + "\\Java\\src\\main\\java\\generated\\"+ jparser));
+        Files.move(Paths.get(basePath+"\\Java\\src\\" + jlexerCarpeta + "\\" + jlexer), Paths.get(basePath + "\\Java\\src\\main\\java\\generated\\" + jlexer));
+
+    }
+
+    public static void PruebasLexerParser() throws Exception{
+        String basePath, fullPathScanner, fullPathParser = "";
+        MainJFlexCup mfjc;
+
+        basePath = System.getProperty("user.dir");
+        fullPathScanner = basePath + "\\Java\\src\\codigoPrueba\\prueba1.txt";
+
+         mfjc = new MainJFlexCup();
+         mfjc.prueba1(fullPathScanner);
 
     }
 
