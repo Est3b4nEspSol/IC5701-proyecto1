14dbf3ae4c07aeaf9f6ac8b30b1a1f3a37d5195a
AlbertoG52 <aguerrero@estudiantec.cr>
2025-12-26 22:07:52 -0600

    Corrección del MainJFlexCup (Fix) Se agregó la línea y columna

--- Java/src/MainJFlexCup.java
+++ Java/src/MainJFlexCup.java
@@ -4,38 +4,33 @@ import java_cup.internal_error;
 import java_cup.parser;
 import java_cup.runtime.Symbol;
 import jflex.exceptions.SilentExit;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 import java.io.*;
 
 public class MainJFlexCup {
 
-    public void iniLexerParser(String rutaLexer, String rutaParser) throws internal_error, Exception{
+    public void iniLexerParser(String rutaLexer, String rutaParser) throws internal_error, Exception {
         GenerateLexer(rutaLexer);
         Generateparser(rutaParser);
     }
 
-    public void GenerateLexer(String ruta) throws IOException, SilentExit{
+    public void GenerateLexer(String ruta) throws IOException, SilentExit {
         String[] strArr = {ruta};
         jflex.Main.generate(strArr);
     }
 
-    public void Generateparser(String ruta) throws internal_error, IOException, Exception{
+    public void Generateparser(String ruta) throws internal_error, IOException, Exception {
         String[] strArr = {ruta};
         java_cup.Main.main(strArr);
     }
 
-    public void prueba(String rutaScanear) throws IOException{
+    public void prueba(String rutaScanear) throws IOException {
         Reader reader = new BufferedReader(new FileReader(rutaScanear));
-        reader.read();
         Lexer lex = new Lexer(reader);
         int i = 0;
         Symbol token;
@@ -53,7 +48,6 @@ public class MainJFlexCup {
                 token = lex.next_token();
 
                 if (token.sym == sym.EOF) {
-                    System.out.println("Cantidad de lexemas encontrados: " + i);
                     break;
                 }
 
@@ -61,9 +55,14 @@ public class MainJFlexCup {
                         ? lex.yytext()
                         : token.value.toString();
 
-                System.out.println("token: " + token.sym + ", valor: " + valor);
+                // Obtener línea y columna
+                int linea = token.left + 1;
+                int columna = token.right + 1;
 
-                writer.write("token: " + token.sym + "lexema: " + valor);
+
+                // Escribir en archivo con línea y columna
+                writer.write(" token: " + token.sym + " lexema: " + valor +
+                        "Línea:" + linea + " Columna:" + columna);
                 writer.newLine();
 
                 i++;
@@ -75,9 +74,9 @@ public class MainJFlexCup {
         } finally {
             try {
                 if (writer != null) writer.close();
-            } catch (IOException ignored) {}
+            } catch (IOException ignored) {
+            }
         }
-
     }
 
     public static void main(String[] args) throws Exception {
